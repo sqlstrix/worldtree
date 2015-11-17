d7976731698138fc32835d28f8a1d9d3b04beb2e
sqlstrix <sqlstrix@gmail.com>
2015-11-17 11:03:13 +0400

    comments

--- WorldTree/src/com/java/strix/Main.java
+++ WorldTree/src/com/java/strix/Main.java
@@ -2,10 +2,12 @@ package com.java.strix;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //новый комментарий
 
 		Tree tree = new Tree();
-
+/*
+ * ещё комментарий тут
+ */
 		while (tree.getBranchesCount() < 10) {
 			tree.grow();
 		}
